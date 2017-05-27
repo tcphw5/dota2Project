fa72cfc3408195c1b75a26876310ca52fdf4857e
tcphw5 <tcphw5@mst.edu>
2017-05-27 04:29:16 -0500

    added postgres database and saving matches into database now. also fixed error where last item was not being counted

--- arctracker.py
+++ arctracker.py
@@ -6,15 +6,42 @@ Created on Sat May 20 19:48:27 2017
 
 @author: tpwin10
 """
-#Flask stuff
+# Flask stuff
 from flask import Flask, render_template, request, url_for
-app = Flask(__name__)
-#
-
+from flask_sqlalchemy import SQLAlchemy
 #arc track stuff
 import dota2api
 from collections import Counter
 import pandas as pd
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:lol@localhost/arcbase'
+db = SQLAlchemy(app)
+
+
+# set up db object for stored games
+class Game3(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    matchID = db.Column(db.String(80), unique=True)
+    item1 = db.Column(db.String(80), unique=False)
+    item2 = db.Column(db.String(80), unique=False)
+    item3 = db.Column(db.String(80), unique=False)
+    item4 = db.Column(db.String(80), unique=False)
+    item5 = db.Column(db.String(80), unique=False)
+    item6 = db.Column(db.String(80), unique=False)
+
+    def __init__(self, matchID, item1, item2, item3, item4, item5, item6):
+        self.matchID = matchID
+        self.item1 = item1
+        self.item2 = item2
+        self.item3 = item3
+        self.item4 = item4
+        self.item5 = item5
+        self.item6 = item6
+
+    def __repr__(self):
+        return '<MatchID %r' & self.matchID
+
+
 
 api = dota2api.Initialise("EECA0D811A31963E8E259DBB8EA055C4")
 
@@ -23,10 +50,11 @@ api = dota2api.Initialise("EECA0D811A31963E8E259DBB8EA055C4")
 #can be changed for any hero later
 #only one person for faster testing
 #389202842, 131588917, 129705032, 128512494,
-arcPlayers =    players = [11984011]
+arcPlayers = players = [11984011]
 #hard coded for now
 arc_id = 113
-#
+
+
 
 @app.route('/')
 def homepage():
@@ -50,10 +78,10 @@ def countItems(players, heroID):
     match_counter = 0
 
 
-    #goes though each players history and filters all the arc warden games into
-    #the arcgames list.
+    # goes though each players history and filters all the arc warden games into
+    # the arcgames list.
     for player in players:
-        #each call gets the last 100 games
+        #each call gets the last 100 TOTAL games (not just arc games)
         try:
             history = api.get_match_history(account_id=player)
             for game in history['matches']:
@@ -64,15 +92,23 @@ def countItems(players, heroID):
             continue
 
 
-    #go through arc warden games one at a time counting items
+    # go through arc warden games one at a time counting items
+    # saves new games to postgres database
     for gameID in games:
         match = api.get_match_details(match_id=gameID)
         for guy in match['players']:
             if guy['hero_id'] == heroID:
                 match_counter += 1
-                for x in range(5):
-                    if guy['item_'+str(x+1)] != 0:
-                        total_Items.append(guy['item_'+str(x+1)+'_name'])
+                items = ["none", "none", "none", "none", "none", "none"]
+                for x in range(6):
+                    if guy['item_'+str(x)] != 0:
+                        total_Items.append(guy['item_'+str(x)+'_name'])
+                        items[x] = guy['item_'+str(x)+'_name']
+                if Game3.query.filter_by(matchID=str(gameID)).first() is None:
+                    newgame = Game3(str(gameID), items[0], items[1], items[2], items[3], items[4], items[5])
+                    db.session.add(newgame)
+                    db.session.commit()
+
                 '''
                 #backpack not counted for now
                 for x in range(3):
